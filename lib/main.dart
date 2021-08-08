bf309aca7b43cd3edc08c13ce1e173b0ea1be0e1
Mohamed Ali <mohamedloutfy97@gmail.com>
2021-08-08 16:38:22 +0200

    Setting up the project and adding an auth screen

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_complete_guide/screens/auth_screen.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Movies',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: AuthScreen(),
+    );
+  }
+}
+
+
