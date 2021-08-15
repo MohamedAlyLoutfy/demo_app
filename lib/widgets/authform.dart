d26bec15b1f9bb21d041d0176f8ad8fee2bf3230
Mohamed Ali <mohamedloutfy97@gmail.com>
2021-08-15 11:40:42 +0200

    adding animation to the authentication form

--- lib/widgets/authform.dart
+++ lib/widgets/authform.dart
@@ -12,7 +12,8 @@ class AuthCard extends StatefulWidget {
   _AuthCardState createState() => _AuthCardState();
 }
 
-class _AuthCardState extends State<AuthCard> {
+class _AuthCardState extends State<AuthCard>
+    with SingleTickerProviderStateMixin {
   final GlobalKey<FormState> _formKey = GlobalKey();
   AuthMode _authMode = AuthMode.Login;
   Map<String, String> _authData = {
@@ -23,6 +24,30 @@ class _AuthCardState extends State<AuthCard> {
   };
   var _isLoading = false;
   final _passwordController = TextEditingController();
+  AnimationController _controller;
+  Animation<Offset> _slideAnimation;
+  Animation<double> _opacityAnimation;
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      vsync: this,
+      duration: Duration(milliseconds: 300),
+    );
+    _slideAnimation =
+        Tween<Offset>(begin: Offset(0, -1.5), end: Offset(0, 0)).animate(
+      CurvedAnimation(parent: _controller, curve: Curves.fastOutSlowIn),
+    );
+    _opacityAnimation = Tween(begin: 0.0, end: 1.0)
+        .animate(CurvedAnimation(parent: _controller, curve: Curves.easeIn));
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+    _controller.dispose();
+  }
 
   Future<void> _submit() async {
     if (!_formKey.currentState.validate()) {
@@ -65,24 +90,17 @@ class _AuthCardState extends State<AuthCard> {
     });
   }
 
-  //   void validateAndSave() {
-  //   final FormState form = _formKey.currentState;
-  //   if (form.validate()) {
-  //     print('Form is valid');
-  //   } else {
-  //     print('Form is invalid');
-  //   }
-  // }
-
   void _switchAuthMode() {
     if (_authMode == AuthMode.Login) {
       setState(() {
         _authMode = AuthMode.Signup;
       });
+      _controller.forward();
     } else {
       setState(() {
         _authMode = AuthMode.Login;
       });
+      _controller.reverse();
     }
   }
 
@@ -111,7 +129,9 @@ class _AuthCardState extends State<AuthCard> {
         borderRadius: BorderRadius.circular(10.0),
       ),
       elevation: 8.0,
-      child: Container(
+      child: AnimatedContainer(
+        duration: Duration(milliseconds: 300),
+        curve: Curves.easeIn,
         height: _authMode == AuthMode.Signup ? 500 : 300,
         constraints:
             BoxConstraints(minHeight: _authMode == AuthMode.Signup ? 320 : 260),
@@ -190,22 +210,50 @@ class _AuthCardState extends State<AuthCard> {
                   },
                 ),
 
-                if (_authMode == AuthMode.Signup)
-                  TextFormField(
-                    enabled: _authMode == AuthMode.Signup,
-                    decoration: InputDecoration(labelText: 'Confirm Password'),
-                    obscureText: true,
-                    keyboardType: TextInputType.number,
-                    validator: _authMode == AuthMode.Signup
+                // if (_authMode == AuthMode.Signup)
+                //   TextFormField(
+                //     enabled: _authMode == AuthMode.Signup,
+                //     decoration: InputDecoration(labelText: 'Confirm Password'),
+                //     obscureText: true,
+                //     keyboardType: TextInputType.number,
+                //     validator: _authMode == AuthMode.Signup
 
-                        // ignore: missing_return
-                        ? (value) {
-                            if (value != _passwordController.text) {
-                              return 'Passwords do not match!';
-                            }
-                          }
-                        : null,
+                //         // ignore: missing_return
+                //         ? (value) {
+                //             if (value != _passwordController.text) {
+                //               return 'Passwords do not match!';
+                //             }
+                //           }
+                //         : null,
+                //   ),
+                AnimatedContainer(
+                  constraints: BoxConstraints(
+                      minHeight: _authMode == AuthMode.Signup ? 60 : 0,
+                      maxHeight: _authMode == AuthMode.Signup ? 120 : 0),
+                  duration: Duration(milliseconds: 300),
+                  curve: Curves.easeIn,
+                  child: FadeTransition(
+                    opacity: _opacityAnimation,
+                    child: SlideTransition(
+                      position: _slideAnimation,
+                      child: TextFormField(
+                        enabled: _authMode == AuthMode.Signup,
+                        decoration:
+                            InputDecoration(labelText: 'Confirm Password'),
+                        obscureText: true,
+                        validator: _authMode == AuthMode.Signup
+                            // ignore: missing_return
+                            ? (value) {
+                                if (value != _passwordController.text) {
+                                  return 'Passwords do not match!';
+                                }
+                              }
+                            : null,
+                      ),
+                    ),
                   ),
+                ),
+
                 SizedBox(
                   height: 20,
                 ),
